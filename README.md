d80adf2026f4d03a0e4706900500157a2b29c0a4
unknown <ashuisalive@gmail.com>
2024-07-30 01:28:04 +0530

    chapter 2 completed

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 Namaste React 
+
+Parcel
+- Dev build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithms - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unsed code
+- Different dev and prod Bundles
+
+While building using Parcel
+- Command = npx parcel build index.html
+  Before running this command remove "main": "App.js" from package.json otherwise it will throw an error, because it's automatically generated for you.
+
+browsersList: [
+    "Last 2 Chrome version",
+    "Last 2 Firefox version",
+    "Last 2 versions"
+]
+
+Check official documentation and github readme file
--- app.js
+++ app.js
@@ -1,20 +1,27 @@
-// ReactElement(Object) = HTML(Browser Understands)
+//  *<div id="parent">
+//     <div id="child1">
+//         <h1>I'm h1 tag</h1>
+//         <h2>I'm h2 tag</h2>
+//     </div>
+//     <div id="child2">
+//         <h1>I'm h1 tag</h1>
+//         <h2>I'm h1 tag</h2>
+//     </div>
+//  </div>
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child1" }, [
-        React.createElement("h1", {}, "I am a h1 tag"),
-        React.createElement("h2", {}, "I am a h2 tag")
-    ]),
-    React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "I am a h1 tag"),
-        React.createElement("h2", {}, "I am a h2 tag")
-    ])
-])
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);
 
-// JSX
-
-console.log(parent); // object
-
-const root = React.createRoot(document.getElementById("header"));
-
+console.log("parent", parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
